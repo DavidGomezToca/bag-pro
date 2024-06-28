ce76eded0a76d6f104011a499d48099281f151b6
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-06-28 13:58:48 +0200

    BAG PRO 0.3.0
    Update App.js
    Update index.css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -5,7 +11,7 @@ export default function App() {
       <Form />
       <PackingList />
       <Stats />
-      <VerisonWatermark/>
+      <VerisonWatermark />
     </div>
   )
 }
@@ -27,8 +33,21 @@ function Form() {
 function PackingList() {
   return (
     <div className="list">
-      LIST
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
     </div>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   )
 }
 
