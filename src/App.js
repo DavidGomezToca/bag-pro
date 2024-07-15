ad4b8a0ca37ca2ef62f5387386d507b0ab8613fe
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-07-15 18:42:09 +0200

    BAG PRO 0.8.0
    Add Toggle Packed Items Feature
    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
@@ -14,14 +14,18 @@ export default function App() {
   }
 
   function handleDeleteItem(id) {
-    setItems(items => items.filter(item => item.id !== id))
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems(items => items.map(item => item.id === id ? { ...item, packed: !item.packed } : item));
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
       <Stats />
       <VerisonWatermark />
     </div>
@@ -60,20 +64,21 @@ function Form({ onAddItems }) {
   )
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   console.log(items)
   return (
     <div className="list">
       <ul>
-        {items.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />)}
+        {items.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />)}
       </ul>
     </div>
   )
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={() => { onToggleItem(item.id) }} />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
