70aa8557ff0b4e664c1dcdaa8f2cfe38bb4dea1a
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-07-15 19:11:29 +0200

    BAG PRO 0.9.0
    Add Stats Feature
    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
       <VerisonWatermark />
     </div>
   )
@@ -46,7 +46,6 @@ function Form({ onAddItems }) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
     onAddItems(newItem);
     setDescription("");
     setQuantity(1);
@@ -65,7 +64,6 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
-  console.log(items)
   return (
     <div className="list">
       <ul>
@@ -87,10 +85,16 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   )
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) return <p className="stats"><em>Start adding some items to your packing list ✈</em></p>
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round(numPacked / numItems * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X.(X%)</em>
+      <em>{percentage === 100 ? "You got everything! Ready to go!" : `You have ${numItems} items on your list, and you already packed ${numPacked}. (${percentage}%)`}</em>
     </footer>
   )
 }
